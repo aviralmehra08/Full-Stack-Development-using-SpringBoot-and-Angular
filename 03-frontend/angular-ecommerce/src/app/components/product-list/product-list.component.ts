276f4003a7735deaabbcd069309b4365ff7c1317
Aviral Mehra <aviral.mehra008@gmail.com>
2023-05-31 15:07:42 +0530

    Search for Products by Keyword

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -14,6 +14,8 @@ export class ProductListComponent implements OnInit{
   
   products:Product[] = [];
   currentCategoryId:number = 1;
+  searchMode:boolean = false;
+
   constructor(private productService:ProductService,
               private route: ActivatedRoute){}
 
@@ -24,7 +26,23 @@ export class ProductListComponent implements OnInit{
   }
 
   listProducts() {
-
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+    if(this.searchMode){
+      this.handleSearchProducts();
+    }
+    else{
+      this.handleListProducts();
+    }
+  }
+  handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+    //now searhc for products using keyword
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      });
+  }
+  handleListProducts(){
     //check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
     if(hasCategoryId) {
@@ -41,5 +59,4 @@ export class ProductListComponent implements OnInit{
       }
     )
   }
-
 }
