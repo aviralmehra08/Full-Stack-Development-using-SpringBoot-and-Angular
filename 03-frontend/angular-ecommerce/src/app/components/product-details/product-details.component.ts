2490f0ae20296aa55925049389778b8e7013a5d0
Aviral Mehra <aviral.mehra008@gmail.com>
2023-06-02 16:07:09 +0530

    Shopping Cart Status Component

--- 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -11,7 +13,7 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductDetailsComponent implements OnInit {
   product!: Product;
   
-  constructor(private productService: ProductService, private route: ActivatedRoute){}
+  constructor(private productService: ProductService, private route: ActivatedRoute, private cartService: CartService){}
 
 
   ngOnInit(): void{
@@ -26,5 +28,10 @@ export class ProductDetailsComponent implements OnInit {
         this.product = data;
       }
     )
+  }
+  addtoCart(){
+    console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
   }
 }
--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -24,6 +26,7 @@ export class ProductListComponent implements OnInit{
   previousKeyword:string = "";
 
   constructor(private productService:ProductService,
+              private cartService: CartService,
               private route: ActivatedRoute){}
 
   ngOnInit(){
@@ -96,5 +99,12 @@ export class ProductListComponent implements OnInit{
       this.thePageSize=data.page.size;
       this.theTotalElements = data.page.totalElements;
     }
+  }
+  addtoCart(theProduct:Product){
+    console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+
+    const theCartItem = new CartItem(theProduct);
+    this.cartService.addToCart(theCartItem);
+
   }
 }
